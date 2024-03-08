f2e1b468f4b07a2192629d21da4cec6a59a3193d
Jorge Hernández <jorgeandres9623@gmail.com>
2024-03-08 13:04:45 -0600

    Se eliminó la parte del front para hacer pruebas, corriendolos de maneras independientes.

--- src/main/java/com/copaymentprueba/copaymentprueba/controller/ExcelUsuarioController.java
+++ src/main/java/com/copaymentprueba/copaymentprueba/controller/ExcelUsuarioController.java
@@ -10,15 +10,13 @@ import com.copaymentprueba.copaymentprueba.service.ExcelUsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/excel/")
+//@CrossOrigin(origins = "http://localhost:8080")
 public class ExcelUsuarioController {
 
     @Autowired
@@ -43,7 +41,7 @@ public class ExcelUsuarioController {
     }
 
 
-    @PostMapping("getExcelUsuariosv2")
+    @PostMapping("getExcelUsuariosV2")
     public ResponseEntity<GetExcelUsuariosResponseDto> getExcelUsuariosV2(@RequestBody GetExcelUsuariosRequestDto requestDto) {
         GetExcelUsuariosResponseDto responseDto = new GetExcelUsuariosResponseDto();
         List<ExcelUsuariosResponseEntity> excelUsuariosNoValidados = excelUsuarioService.validarExcelUsuariosEntities(requestDto.getExcelInfo());
